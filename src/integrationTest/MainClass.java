1fea0acf0d37a07a8a1fa4f6c6b673b08f0df24d
jolene.costa <jcosta@sonatype.com>
2019-10-23 10:43:41 +0100

    First commit from Eclipse

--- src/integrationTest/MainClass.java
+++ src/integrationTest/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello there!");
+		System.out.print("hello there people!");
 	}
 
 }
